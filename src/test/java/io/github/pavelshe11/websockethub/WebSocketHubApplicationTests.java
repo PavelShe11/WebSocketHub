37bc7c1b90b42e1426cfedc8516948793b71f456
pa.sheludyakov <pasha.technician@bk.ru>
2025-09-05 00:27:36 +0300

    init commit

---
+++ src/test/java/io/github/pavelshe11/websockethub/WebSocketHubApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.pavelshe11.websockethub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSocketHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
